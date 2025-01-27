a738a2737c1edbe68ecb22e17b4af85be1de9f12
MarkCesium <danikhrenkov29@gmail.com>
2025-01-27 19:10:26 +0300

    Fix metadata for alembic

--- migrations/env.py
+++ migrations/env.py
@@ -7,7 +7,7 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 
-from src.core.database.model import BaseModel
+from src.models import BaseModel
 from src.core.config import settings
 
 # this is the Alembic Config object, which provides
