53f7739e40d4fff0f7747f3fcc0a3278925d4ba9
MarkCesium <danikhrenkov29@gmail.com>
2025-01-28 00:12:37 +0300

    Move and rename fastapi_users_routers

--- src/api/dependencies/authentication/fastapi_users_routers.py
+++ src/api/dependencies/authentication/fastapi_users_routers.py
@@ -5,7 +5,7 @@ from src.core.types.user_id import UserIDType
 from .user_manager import get_user_manager
 from .backend import auth_backend
 
-fastapi_users = FastAPIUsers[User, UserIDType](
+fastapi_users_routers = FastAPIUsers[User, UserIDType](
     get_user_manager,
     [auth_backend],
 )
