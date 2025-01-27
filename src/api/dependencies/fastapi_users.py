c93d4727456eebcaac869fed23fc049950fe6aaa
MarkCesium <danikhrenkov29@gmail.com>
2025-01-28 00:01:22 +0300

    Create fastapi_users helper

---
+++ src/api/dependencies/fastapi_users.py
@@ -0,0 +1,11 @@
+from fastapi_users import FastAPIUsers
+
+from src.models import User
+from src.core.types.user_id import UserIDType
+from .user_manager import get_user_manager
+from .backend import auth_backend
+
+fastapi_users = FastAPIUsers[User, UserIDType](
+    get_user_manager,
+    [auth_backend],
+)
