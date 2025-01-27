3c9ef625810b8ffc2cee6e89a08c8c2c413e7c93
MarkCesium <danikhrenkov29@gmail.com>
2025-01-28 00:17:42 +0300

    Fix var

--- src/api/dependencies/authentication/fastapi_users_routers.py
+++ src/api/dependencies/authentication/fastapi_users_routers.py
@@ -3,9 +3,9 @@ from fastapi_users import FastAPIUsers
 from src.models import User
 from src.core.types.user_id import UserIDType
 from .user_manager import get_user_manager
-from .backend import auth_backend
+from .backend import authentication_backend
 
 fastapi_users_routers = FastAPIUsers[User, UserIDType](
     get_user_manager,
-    [auth_backend],
+    [authentication_backend],
 )
