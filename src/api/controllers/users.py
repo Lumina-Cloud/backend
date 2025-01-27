ce4ea5a6aac820aeac5b21c6f46d80e0c3195830
MarkCesium <danikhrenkov29@gmail.com>
2025-01-28 00:46:34 +0300

    Add users routes

---
+++ src/api/controllers/users.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter
+
+from src.api.dependencies.authentication.fastapi_users_routers import (
+    fastapi_users_routers,
+)
+from src.api.schemas.user import UserRead, UserUpdate
+
+router = APIRouter(prefix="/users", tags=["Users"])
+
+router.include_router(
+    router=fastapi_users_routers.get_users_router(UserRead, UserUpdate),
+)
