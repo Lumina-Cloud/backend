61d6ccdc8990314e93fe37ddbc86b1d6a3b23adc
MarkCesium <danikhrenkov29@gmail.com>
2025-01-28 00:44:47 +0300

    Add /register route

--- src/api/controllers/auth.py
+++ src/api/controllers/auth.py
@@ -4,9 +4,19 @@ from src.api.dependencies.authentication.fastapi_users_routers import (
     fastapi_users_routers,
 )
 from src.api.dependencies.authentication.backend import authentication_backend
+from src.api.schemas.user import UserRead, UserCreate
 
 router = APIRouter(prefix="/auth", tags=["Authentication"])
 
 router.include_router(
-    router=fastapi_users_routers.get_auth_router(authentication_backend),
+    router=fastapi_users_routers.get_auth_router(
+        authentication_backend,
+    ),
+)
+
+router.include_router(
+    router=fastapi_users_routers.get_register_router(
+        UserRead,
+        UserCreate,
+    )
 )
