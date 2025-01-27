6f7ec1e18ab1cdb5592b2f94af03214fa6f4bd79
MarkCesium <danikhrenkov29@gmail.com>
2025-01-28 00:56:36 +0300

    Formatting

--- src/api/controllers/users.py
+++ src/api/controllers/users.py
@@ -8,5 +8,8 @@ from src.api.schemas.user import UserRead, UserUpdate
 router = APIRouter(prefix="/users", tags=["Users"])
 
 router.include_router(
-    router=fastapi_users_routers.get_users_router(UserRead, UserUpdate),
+    router=fastapi_users_routers.get_users_router(
+        UserRead,
+        UserUpdate,
+    ),
 )
