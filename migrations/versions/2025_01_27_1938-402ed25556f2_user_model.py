73f7694852afb9a8db8ea5369959c93467bbe9d4
MarkCesium <danikhrenkov29@gmail.com>
2025-01-27 19:40:25 +0300

    Create User migration

---
+++ migrations/versions/2025_01_27_1938-402ed25556f2_user_model.py
@@ -0,0 +1,38 @@
+"""User model
+
+Revision ID: 402ed25556f2
+Revises: 
+Create Date: 2025-01-27 19:38:55.850995
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "402ed25556f2"
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "user",
+        sa.Column("id", sa.Integer(), autoincrement=True, nullable=False),
+        sa.Column("email", sa.String(length=320), nullable=False),
+        sa.Column("hashed_password", sa.String(length=1024), nullable=False),
+        sa.Column("is_active", sa.Boolean(), nullable=False),
+        sa.Column("is_superuser", sa.Boolean(), nullable=False),
+        sa.Column("is_verified", sa.Boolean(), nullable=False),
+        sa.PrimaryKeyConstraint("id"),
+    )
+    op.create_index(op.f("ix_user_email"), "user", ["email"], unique=True)
+
+
+def downgrade() -> None:
+    op.drop_index(op.f("ix_user_email"), table_name="user")
+    op.drop_table("user")
